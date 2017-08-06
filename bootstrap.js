c88be7d4c0a008da40d28257c5895b17b221ec23
Hartley Brody <hartley.brody@gmail.com>
2017-08-06 11:31:46 -0400

    do a basic search for the page's hostname in history (seems to be scoped)

--- bootstrap.js
+++ bootstrap.js
@@ -1,11 +1,26 @@
 // when the extension is first installed
 chrome.runtime.onInstalled.addListener(function(details) {
-    chrome.storage.sync.set({clean_news_feed: true});
+    console.log("just installed")
 });
 
 // listen for any changes to the URL of any tab.
 chrome.tabs.onUpdated.addListener(function(id, info, tab){
-    if (tab.url.toLowerCase().indexOf("example.com") > -1){
-        chrome.pageAction.show(tab.id);
+
+    if (info.status !== "complete"){
+        return;
     }
+
+    var url = tab.url.toLowerCase()
+
+    // use browser to parse hostname
+    var tmp = document.createElement("a");
+    tmp.href = url;
+    hostname = tmp.hostname
+
+    console.log("checking browsing history for " + hostname)
+
+    chrome.history.search({text: hostname}, function(history){
+        console.log(history)
+    })
+
 });
