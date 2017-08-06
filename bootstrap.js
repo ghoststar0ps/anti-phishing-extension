0fe61088d1c564bfe7f831cb28b8085be256f4bd
Hartley Brody <hartley.brody@gmail.com>
2017-08-06 11:52:08 -0400

    iterate through the history items and tally their stats

--- bootstrap.js
+++ bootstrap.js
@@ -20,7 +20,22 @@ chrome.tabs.onUpdated.addListener(function(id, info, tab){
     console.log("checking browsing history for " + hostname)
 
     chrome.history.search({text: hostname}, function(history){
-        console.log(history)
+
+        history_summary = {
+            total_visits: 0,
+            earliest_visit: 9e15,
+        }
+
+        for (var i=0; i<history.length; i++){
+            var h = history[i]
+
+            history_summary.total_visits += h.visitCount
+            if (h.lastVisitTime < history_summary.earliest_visit){
+                history_summary.earliest_visit = h.lastVisitTime
+            }
+        }
+
+        console.log(history_summary)
     })
 
 });
